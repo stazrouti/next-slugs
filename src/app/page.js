c979043353c4fe4543ac1b3a6168db9cb12476a6
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-01-10 21:08:11 +0100

    add tailwindcss style

--- src/app/page.js
+++ src/app/page.js
@@ -1,41 +1,60 @@
 "use client";
 import Link from 'next/link';
+
 export default function Home() {
   return (
-    <>
-      <p>Blogs using files name</p>
-      <div>
+    <div className="p-8">
+      <p className="text-2xl font-bold mb-4">Blogs using files name</p>
+
+      <div className="mb-4">
         <Link href="/Blogs/Blog1">
-          Go to Blog1
-        </Link>
-      </div>
-      <div>
-        <Link href="/Blogs/Blog2">
-          Go to Blog2
-        </Link>
-      </div>
-      <div>
-        <Link href="/Blogs/Blog3">
-          Go to Blog3
+          <div className="text-blue-500 hover:underline cursor-pointer">
+            Go to Blog1
+          </div>
         </Link>
       </div>
 
-      <p>Blogs using slugs name</p>
-      <div>
+      <div className="mb-4">
+        <Link href="/Blogs/Blog2">
+          <div className="text-blue-500 hover:underline cursor-pointer">
+            Go to Blog2
+          </div>
+        </Link>
+      </div>
+
+      <div className="mb-4">
+        <Link href="/Blogs/Blog3">
+          <div className="text-blue-500 hover:underline cursor-pointer">
+            Go to Blog3
+          </div>
+        </Link>
+      </div>
+
+      <p className="text-2xl font-bold my-4">Blogs using slugs name</p>
+
+      <div className="mb-4">
         <Link href="/posts/Blog1">
-          Go to Blog1
+          <div className="text-blue-500 hover:underline cursor-pointer">
+            Go to Blog1
+          </div>
         </Link>
       </div>
-      <div>
+
+      <div className="mb-4">
         <Link href="/posts/Blog2">
-          Go to Blog2
+          <div className="text-blue-500 hover:underline cursor-pointer">
+            Go to Blog2
+          </div>
         </Link>
       </div>
-      <div>
+
+      <div className="mb-4">
         <Link href="/posts/Blog3">
-          Go to Blog3
+          <div className="text-blue-500 hover:underline cursor-pointer">
+            Go to Blog3
+          </div>
         </Link>
       </div>
-    </>
-  )
+    </div>
+  );
 }
